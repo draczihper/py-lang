63f39fe1439015b30f03843ee3de40bf6c24d45a
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2025-11-19 20:38:58 +0300

    Add result url

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -3,16 +3,16 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from!'
 
 @app.route('/search4', methods=['POST']) 
-def do_search() -> str:
+def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    return str(search4letters(phrase, letters))
+    title = 'Here are you results: '
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html', the_title = title, the_phrase = phrase, the_letters = letters, the_results = results)
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title = "Welcome to search4letters on the web")
